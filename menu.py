279ad1b466b3e1ed158dbf51817bbd0b17ddef5c
Elvira A.Kulikova <e.kulikova@norsi-trans.ru>
2021-04-01 16:14:03 +0300

    user menu

--- menu.py
+++ menu.py
@@ -1,5 +1,14 @@
 from app import books
 
+USER_CHOISE = ''' Enter one of the following
+
+- 'b' to look at 5-star books
+- 'c' to look at the cheapest books
+- 'n' to just get the next available book on the catalogue
+- 'q' to exit
+
+Enter your choice:'''
+
 def print_best_books():
     best_books = sorted(books, key=lambda x: x.rating * -1)[:10]
     for book in best_books:
@@ -10,7 +19,26 @@ def print_cheapest_books():
     for book in cheapest_books:
         print(book)
 
-print("==== Best ===")
-print_best_books()
-print("==== Cheapest ===")
-print_cheapest_books()
+books_generator = (x for x in books)
+
+def get_next_book():
+    print(next(books_generator))
+
+
+user_choices = {
+    'b': print_best_books,
+    'c': print_cheapest_books,
+    'n': get_next_book
+}
+
+
+def menu():
+    user_input = input(USER_CHOISE)
+    while user_input != 'q':
+        if user_input in ['b', 'c', 'n']:
+            user_choices[user_input]()
+        else:
+            print('Please choose a valid command.')
+        user_input = input(USER_CHOISE)
+
+menu()
